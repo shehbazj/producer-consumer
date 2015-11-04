69b6410bb65fc9a5ca66f64af59783057436e57c
Shehbaz Jaffer <shehbazjaffer007@gmail.com>
2015-11-03 20:38:03 -0500

    Multiple producer consumer with single mutex. TODO Redefine QUEUE_SIZE and CONCURRENT_OPS operations

--- prod_con.c
+++ prod_con.c
@@ -14,21 +14,17 @@ Thread 2 and 3 read data from the array and delete the data
 #define QUEUE_SIZE 10 
 #define NUM_PTHREADS 5 
 #define NUM_CTHREADS 5 
+#define CONCURRENT_OPS 4
 
 typedef struct args{
-	pthread_mutex_t *mutex;
-	pthread_cond_t *cv;	
-	int *count;
+	pthread_mutex_t *mutex_count;	// binary counter to ensure atomic operation on count variable
+	pthread_cond_t *cv;		// Upto CONCURRENT_OPS 
+	int *count;			// can operate on queue at the same time. 
 }args;
-
-typedef struct con_args{
-	pthread_mutex_t *mutex;
-	pthread_cond_t *cv;
-}con_args;
 
 void copy_args(void *arg, struct args *p)
 {
-	p->mutex = ((args *)arg)->mutex;
+	p->mutex_count = ((args *)arg)->mutex_count;
 	p->cv = ((args *)arg)->cv;
 	p->count = ((args *)arg)->count;
 	return;
@@ -50,23 +46,21 @@ static void * prod(void *arg)
 
 	/* try getting exclusive access to p.count variable */
 
-	pthread_mutex_lock(p->mutex);
-
-	/* no other thread can get here unless current thread unlocks mutex */
-	while(*(p->count) < 1 ){	/*if count is 0, queue is overloaded with requests */
-		pthread_cond_wait(p->cv, p->mutex);
-				/* wait on cv and unlock mutex. Other threads may now access p.count*/
+	pthread_mutex_lock(p->mutex_count);
+	while(*(p->count) <= 0){
+		pthread_cond_wait(p->cv, p->mutex_count);
 	}
-	/* program reached here, that means atleast one of the queue elements is empty */
-	/* TODO place data on queue here */
 
-	//printf("%s():BEFORE decrementing count = %d tid = %ld\n",__func__, *(p->count), syscall(SYS_gettid));
-	
-	produce();
 	*(p->count) = *(p->count)- 1; /* One more queue element filled */		
-	//printf("%s():AFTER decrementing count = %d tid = %ld\n",__func__, *(p->count), syscall(SYS_gettid));
+	pthread_mutex_unlock(p->mutex_count);
 
-	pthread_mutex_unlock(p->mutex);
+	produce();
+
+	pthread_mutex_lock(p->mutex_count);
+	if(*(p->count) < CONCURRENT_OPS){
+		*(p->count) = *(p->count) + 1; /* One more queue element filled */		
+	}
+	pthread_mutex_unlock(p->mutex_count);
 	free(p);
 	return NULL;
 }
@@ -78,19 +72,18 @@ static void * con(void *arg)
 	copy_args(arg, c);
 	
 	/* Try to get exclusive access to count */
-	pthread_mutex_lock(c->mutex);
-
+	pthread_mutex_lock(c->mutex_count);
+	if(*(c->count) < QUEUE_SIZE){
+		* (c->count)  = *(c->count) + 1;
+		consume();
+	}
+	pthread_mutex_unlock(c->mutex_count);
 	/* wakeup other threads that may be waiting on the queue */
 	pthread_cond_signal(c->cv);
 
-	if(*(c->count) < QUEUE_SIZE){
-		* (c->count)  = *(c->count) + 1;
-	}
-
 	/*TODO start processing data here*/
 	//printf("%s(): count = %d tid = %ld\n",__func__, *(c->count), syscall(SYS_gettid));
-	consume();
-	pthread_mutex_unlock(c->mutex);
+//	pthread_mutex_unlock(c->mutex);
 	free(c);
 	return NULL;
 }
@@ -98,7 +91,7 @@ static void * con(void *arg)
 int main()
 {
 	pthread_t thread_p[NUM_PTHREADS], thread_c[NUM_CTHREADS];
-	pthread_mutex_t mutex;
+	pthread_mutex_t mutex_count;
 	pthread_attr_t attr;
 	pthread_cond_t cv;
 	void *res;
@@ -107,14 +100,14 @@ int main()
 	int i, count = QUEUE_SIZE;
 
 	pthread_attr_init(&attr);
-	pthread_mutex_init(&mutex, NULL);
+	pthread_mutex_init(&mutex_count, NULL);
 	pthread_cond_init(&cv, NULL);	
 	
 	// create producer args
 
 	struct args *params;
 	params = malloc(sizeof(args));
-	params->mutex = &mutex;
+	params->mutex_count = &mutex_count;
 	params->cv = &cv;
 	params->count = malloc(sizeof(int));
 	*(params->count) = QUEUE_SIZE;	
@@ -137,7 +130,7 @@ int main()
 
 	free(params->count);
 	free(params);
-	pthread_mutex_destroy(&mutex);
+	pthread_mutex_destroy(&mutex_count);
 	pthread_cond_destroy(&cv);
 
 	return 0;
